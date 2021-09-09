0d0941fd849f5d427c3348d69825734c72ede3fa
Jhon Carlos Condori Cuevas <www.hackdcjcc@hotmail.com>
2021-09-08 20:48:23 -0400

    Agregar archivos de proyecto.

---
+++ Source.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << " Condori Cuevas Jhon Carlos " << endl;
+	cout << " CU: 35-4722 " << endl;
+	cout << " CI: 13123129 " << endl;
+	cout << " Ing. de Sistemas " << endl;
+}
